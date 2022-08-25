bfa76b84949e2e7dd468e45216300cb31e172840
AD <liyoujun600@gmail.com>
2022-08-25 23:10:43 +0800

    添加樣式

---
+++ docs/javascripts/extra.js
@@ -0,0 +1,3 @@
+document.onload =function () {
+    document.getElementsByClassName("md-footer-copyright")[0].innerHTML = `<div class="md-footer-copyright__highlight">Copyright © 2021 - 2022 AD</div>`
+}
