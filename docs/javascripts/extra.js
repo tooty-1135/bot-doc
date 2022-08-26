3243ba132710712ab09e0ecaabe62e1116d99e2f
AD <liyoujun600@gmail.com>
2022-08-26 10:41:30 +0800

    Update extra.js

--- docs/javascripts/extra.js
+++ docs/javascripts/extra.js
@@ -1,3 +1,6 @@
-document.onload =function () {
+function reset() {
     document.getElementsByClassName("md-footer-copyright")[0].innerHTML = `<div class="md-footer-copyright__highlight">Copyright © 2021 - 2022 AD</div>`
 }
+window.onload = function() { //等待網頁載入完成
+    reset()
+};
